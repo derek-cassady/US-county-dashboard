d6fc59523dbf2249055fb68f068943e89b0902b5
Derek Cassady <derek@cassadyweb.com>
2023-07-18 12:41:37 -0400

    updated main.py to run 3 notebooks

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
+'''Open and run the agesex notebook'''
 filename = 'agesex.ipynb'
-with open(filename) as ff:
-    nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
+with open(filename) as aa:
+    nb_in = nbformat.read(aa, nbformat.NO_CONVERT)
+    
+ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+nb_out = ep.preprocess(nb_in)
+
+'''Open and run the econ notebook'''
+filename = 'econ.ipynb'
+with open(filename) as bb:
+    nb_in = nbformat.read(bb, nbformat.NO_CONVERT)
+    
+ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+nb_out = ep.preprocess(nb_in)
+
+
+'''Open and run the income and benefits notebook'''
+filename = 'income_benefit.ipynb'
+with open(filename) as cc:
+    nb_in = nbformat.read(cc, nbformat.NO_CONVERT)
     
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
 
