9a3bc87ab0fbe073c9d27f5c042a4ee2b0774358
Derek Cassady <derek@cassadyweb.com>
2023-07-23 00:48:24 -0400

    Testing new direction for app

---
+++ dash_app/pages/pg3.py
@@ -0,0 +1,11 @@
+import dash
+from dash import dcc, html
+
+# Page Registry
+dash.register_page(__name__)
+
+# layout = html.Div(
+#     [
+#         dcc.Markdown('# This will be the content of Page 3')
+#     ]
+# )
